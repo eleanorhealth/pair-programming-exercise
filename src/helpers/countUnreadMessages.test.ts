eba84de5b858848011dd7afc739a760b16213ad1
Kyle Odell <kyle.odell@eleanorhealth.com>
2021-07-21 14:51:23 -0400

    Empty implementation with tests

---
+++ src/helpers/countUnreadMessages.test.ts
@@ -0,0 +1,14 @@
+import { bethZell, calvinHobbes, isaiahWilliams } from "mocks/members";
+import { countUnreadMessages } from "./countUnreadMessages";
+
+describe.skip("countUnreadMessages", () => {
+  it("counts read: false", () => {
+    expect(countUnreadMessages(isaiahWilliams.messages)).toEqual(1);
+  });
+  it("does not count read: true", () => {
+    expect(countUnreadMessages(calvinHobbes.messages)).toEqual(2);
+  });
+  it("returns zero when no messages", () => {
+    expect(countUnreadMessages(bethZell.messages)).toEqual(0);
+  });
+});
---
+++ src/helpers/countUnreadMessages.ts
@@ -0,0 +1,5 @@
+import { Message } from "types/member";
+
+export const countUnreadMessages = (messages: Message[]): number => {
+  return -1;
+};
