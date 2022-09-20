a0173c13e87d7cab3bc171dbf534eba35a6bf276
Adam Perkins <adam.perkins@eleanorhealth.com>
2022-09-20 15:43:06 -0400

    Use double quotes

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
   resolver: "<rootDir>/src/jest/resolver.js",
   roots: ["<rootDir>/src"],
   modulePaths: ["<rootDir>/src"],
